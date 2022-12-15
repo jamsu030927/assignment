87ce50c9fb814d5133601be55b9918a75d4f286a
jamsu030927 <120685104+jamsu030927@users.noreply.github.com>
2022-12-16 06:28:54 +0900

    Add files via upload

---
+++ web19201.jsx
@@ -0,0 +1,44 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./web19201.css";
+
+class Web19201 extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="0c618493-0548-459f-b8a2-ae4a162ba3b6" className="web19201">        <div data-layer="b2bfc866-0420-4dfd-8e50-32ef051a3707" className="x568"></div>
+        <div data-layer="577cf1a2-005f-4190-a644-41308cde7156" className="x573">            <div data-layer="d6f9a425-f699-41ff-8734-b8b02427770a" className="x571"></div>
+</div>
+        <div data-layer="e48a91ec-d9e6-41de-a68b-50717b97692e" className="x575">            <div data-layer="b160945b-0079-463c-be05-c208b526ab12" className="x574"></div>
+</div>
+        <div data-layer="313d1113-f510-4fcf-a82c-5a6fb64a1381" className="x577">            <div data-layer="400bdafe-3288-45ed-bd20-c926ef930a72" className="x576"></div>
+</div>
+        <div data-layer="48f134dd-0758-4f2f-bfd9-2b53e47f5e81" className="x570">            <div data-layer="e032834a-df20-4762-a487-d9d4cc6df854" className="x569"></div>
+</div>
+</div>
+
+    );
+  }
+}
+
+Web19201.propTypes = {
+
+}
+
+Web19201.defaultProps = {
+
+}
+
+
+export default Web19201;
+          
